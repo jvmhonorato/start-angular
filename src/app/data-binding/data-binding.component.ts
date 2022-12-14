507b9a38334d87f32d4a171836450f05068abd4e
vituhonorato <vituhonorato.89@gmail.com>
2022-12-13 23:24:53 -0300

    property binding, passing properties dynamically

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class DataBindingComponent {
   public nome:string =  "Victor"
   public idade: number = 33
+  public checkedDisabled: boolean = false
+  public imgTitle:string = "Property Binding"
+  public imgSrc:string ="https://st.depositphotos.com/1004061/3547/i/950/depositphotos_35477449-stock-photo-big-lion-lying-on-savannah.jpg"
 }
