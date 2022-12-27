0784195037ab2c93716fb61f35b3cf948c9f37c3
vituhonorato <vituhonorato.89@gmail.com>
2022-12-26 23:59:07 -0300

    Add event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -11,4 +11,8 @@ export class DataBindingComponent {
   public checkedDisabled: boolean = false
   public imgTitle:string = "Property Binding"
   public imgSrc:string ="https://st.depositphotos.com/1004061/3547/i/950/depositphotos_35477449-stock-photo-big-lion-lying-on-savannah.jpg"
+
+  public alertInfo() {
+    alert("Deu Bom!")
+  }
 }
