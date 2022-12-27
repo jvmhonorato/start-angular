a391e897ca65c5c14f83d146475f82aa66a7fc38
vituhonorato <vituhonorato.89@gmail.com>
2022-12-27 00:52:57 -0300

    add function MouseEventTest() to render the cursor positon in div

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -11,8 +11,15 @@ export class DataBindingComponent {
   public checkedDisabled: boolean = false
   public imgTitle:string = "Property Binding"
   public imgSrc:string ="https://st.depositphotos.com/1004061/3547/i/950/depositphotos_35477449-stock-photo-big-lion-lying-on-savannah.jpg"
-
+  public position: {x:number, y:number} = {x:0, y:0};
   public alertInfo(valor: MouseEvent) {
     alert(valor);
+  }
+
+
+  public mouseMoveTest(valor: MouseEvent){
+    //console.log(valor)
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
   }
 }
