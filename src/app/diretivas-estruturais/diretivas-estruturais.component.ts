de8a142bb1053c4a085b857fbcf1ba8b2f827ae3
vituhonorato <vituhonorato.89@gmail.com>
2022-12-28 00:32:06 -0300

    add condition render with click event using ngIf to show true or false

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diretivas-estruturais',
   templateUrl: './diretivas-estruturais.component.html',
   styleUrls: ['./diretivas-estruturais.component.scss']
 })
-export class DiretivasEstruturaisComponent {
+export class DiretivasEstruturaisComponent implements OnInit{
+  public condition: boolean = true;
+  public conditionClick: boolean = true;
 
+  constructor() {}
+
+  ngOnInit(): void {
+
+    setInterval( () => {
+      if(this.condition){
+        this.condition = false
+      }else{
+        this.condition = true;
+      }
+
+    }, 2000)
+  }
+  public onClick() {
+    if (this.conditionClick) {
+      this.conditionClick = false;
+  }else{
+    this.conditionClick = true;
+  }
+ }
 }
+
+
+
+
+
