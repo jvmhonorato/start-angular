8b99cab6b0fc02173f00bce2e30eeea275af1fca
vituhonorato <vituhonorato.89@gmail.com>
2023-01-07 05:53:57 -0300

    add input component in shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<
+
   <app-data-binding></app-data-binding>
   <app-diretivas-estruturais></app-diretivas-estruturais>
   <app-diretivas-atributos>
@@ -11,18 +11,27 @@ import { Component, OnInit} from '@angular/core';
     <h3>Footer</h3>
   </app-diretivas-atributos>
   <app-new-component></app-new-component>
+  <app-input [contador]= "addValue"></app-input>
+  <button (click)="add()">Add</button>
+  <button (click)="remove()">Remove</button>
   <router-outlet></router-outlet>`
 
 })
 export class AppComponent implements OnInit  {
 
-
+  public addValue: number = 0
 
   constructor() {
 
   }
   ngOnInit(): void {
 
+  }
+  public add() {
+    this.addValue = this.addValue + 1
+  }
+  public remove() {
+    this.addValue -= 1
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
 
 
 
 @NgModule({
   declarations: [
     NewComponentComponent,
+    InputComponent,
 
   ],
-  exports:[NewComponentComponent],
+  exports:[NewComponentComponent,InputComponent],
   imports: [
     CommonModule
   ]
