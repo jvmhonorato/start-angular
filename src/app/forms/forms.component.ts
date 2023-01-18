44c91b9ceb15d5788dc61f5acf5a77705fcbd8b8
vituhonorato <vituhonorato.89@gmail.com>
2023-01-17 21:09:02 -0300

    add submit button and submit method in TS component

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -13,4 +14,10 @@ export class FormsComponent {
     {comida:"sonho", preco:"R$13,00"},
   ]
 
+  public submitForm(form: NgForm){
+
+   if(form.valid){
+    console.log(form.value)
+   }
+  }
 }
