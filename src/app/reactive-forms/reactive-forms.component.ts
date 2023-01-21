b87b614524b63d17fa518a6a2c15796cd6e7a4f1
vituhonorato <vituhonorato.89@gmail.com>
2023-01-20 21:35:00 -0300

    add conditional in submitForm using valid and invalid

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -16,8 +16,12 @@ export class ReactiveFormsComponent {
   constructor(private formBuilder: FormBuilder) { }
 
   public submitForm() {
-    console.log(this.cadastroForm.value);
-    console.log(this.cadastroForm.value.firstName);
+    if(this.cadastroForm.valid){
+      console.log(this.cadastroForm.value);
+      console.log(this.cadastroForm.value.firstName);
+      console.log(this.cadastroForm.value.lastName);
+    }
+
   }
 
 }
